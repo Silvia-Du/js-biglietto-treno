1a19f7b3c9667d501a5fe6128e219d4ec63ec71a
Silvia-Du <silviadu@hotmail.it>
2022-04-06 13:47:33 +0200

    Primo commit: impostazione base di lavoro e flusso di lavoro

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+a- Chiedere all'utente quanti km vuole percorrere
+b- Chiedere l'età all'utente
+c- calcolare il prezzo in base ai km da percorrere e allo sconto previsto dall'età dell'utente.
+d- stampare in pagina il prezzo del biglietto (con un massimo di 2 decimali)
+
+Flusso di lavoro:
+1. inizializzare le variabili
+2. far inserire i dati
+3. calcolare il totale del biglietto
+4. calcolare lo sconto (se previsto) in base all'età
+5.stampare il prezzo del biglietto
+*/
